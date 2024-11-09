a0ff827868eafa1a0ff4f995725c734c7a807533
Levi Utima <levi.utima@gmail.com>
2024-11-09 03:24:14 -0300

    first commit

---
+++ back/src/main/java/com/example/codeReflections/CodeReflectionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeReflections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReflectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeReflectionsApplication.class, args);
+	}
+
+}
---
+++ front/src/pages/home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../components/ui/button";
+
+export function Home() {
+    return(
+        <div className="font-[400px] bg-slate-400">
+            <Button className="">ola</Button>
+            <h1 className="text-blue font-bold text-[30px]">teste</h1>
+        </div>
+    )
+}
